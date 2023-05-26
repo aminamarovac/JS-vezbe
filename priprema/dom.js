57c0f988ff752597cd6770da663ad13d303a718b
aminamarovac <aminamarovac68@gmail.com>
2023-05-26 23:00:03 +0200

    vezba

---
+++ priprema/dom.js
@@ -0,0 +1,27 @@
+// document.getElementsByTagName("p")[0].innerHTML="text dodat pomocu js";
+// //Pomocu innerHTML menjamo sadrzaj elementa;
+
+// document.getElementsByTagName("p")[1].style.color="yellow";
+
+// document.getElementById("treci").style.border="2px solid red";
+
+// document.getElementsByClassName("ok")[0].style.backgroundColor="orange";
+
+// var plavi=document.getElementsByTagName("h3");
+// for(let i=0 ; i<=plavi.length ; i++)
+//     plavi[i].style.color="red";
+
+//   document.querySelector("button").style.backgroundColor="red";
+
+// var sati = Number(prompt("Unesite neki broj,14"));
+
+// if (!Number.isNaN(sati) && sati <= 24 && sati >= 0) {
+//   if (sati < 10) {
+//     console.log("Dobro jutro");
+//   } else if (sati > 18) {
+//     console.log("Dobro vece");
+//   } else {
+//     console.log("Netacan unos");
+// }
+// }
+// console.log(sati)
